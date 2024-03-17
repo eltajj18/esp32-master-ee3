c0b3157b1628e19a6f6ca44b1d1f5a8c22995d4c
eltajj18 <amirli.eltaj18@gmail.com>
2024-03-17 23:46:24 +0100

    solved nRF issue

--- main/main.c
+++ main/main.c
@@ -14,84 +14,34 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "stdint.h"
 
 #include "include/mirf.h"
 
-#if CONFIG_ADVANCED
-void AdvancedSettings(NRF24_t *dev)
-{
-#if CONFIG_RF_RATIO_2M
-    ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 2MBps");
-    Nrf24_SetSpeedDataRates(dev, 1);
-#endif // CONFIG_RF_RATIO_2M
-
-#if CONFIG_RF_RATIO_1M
-    ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 1MBps");
-    Nrf24_SetSpeedDataRates(dev, 0);
-#endif // CONFIG_RF_RATIO_2M
-
-#if CONFIG_RF_RATIO_250K
-    ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 250KBps");
-    Nrf24_SetSpeedDataRates(dev, 2);
-#endif // CONFIG_RF_RATIO_2M
-
-    ESP_LOGW(pcTaskGetName(0), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
-    Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
-}
-#endif // CONFIG_ADVANCED
-// void receiver(void *pvParameters)
+// #if CONFIG_ADVANCED
+// void AdvancedSettings(NRF24_t * dev)
 // {
-//     ESP_LOGI(pcTaskGetName(0), "Start");
-//     NRF24_t dev;
-//     Nrf24_init(&dev);
-//     uint8_t payload = 32;
-//     uint8_t channel = (uint8_t)115;
-//     Nrf24_SetSpeedDataRates(&dev, 0);
+// #if CONFIG_RF_RATIO_2M
+// 	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 2MBps");
+// 	Nrf24_SetSpeedDataRates(dev, 1);
+// #endif // CONFIG_RF_RATIO_2M
 
-//     Nrf24_config(&dev, channel, payload);
+// #if CONFIG_RF_RATIO_1M
+// 	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 1MBps");
+// 	Nrf24_SetSpeedDataRates(dev, 0);
+// #endif // CONFIG_RF_RATIO_2M
 
-//     // Set own address using 5 characters
-//     esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
-//     if (ret != ESP_OK)
-//     {
-//         ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
-//         while (1)
-//         {
-//             vTaskDelay(1);
-//         }
-//     }
+// #if CONFIG_RF_RATIO_250K
+// 	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 250KBps");
+// 	Nrf24_SetSpeedDataRates(dev, 2);
+// #endif // CONFIG_RF_RATIO_2M
 
-//     // Print settings
-//     Nrf24_printDetails(&dev);
-//     ESP_LOGI(pcTaskGetName(0), "Listening...");
-
-//     uint8_t buf[32];
-
-//     // Clear RX FiFo
-//     while (1)
-//     {
-//         if (Nrf24_dataReady(&dev) == false)
-//             break;
-//         Nrf24_getData(&dev, buf);
-//     }
-
-//     while (1)
-//     {
-//         // When the program is received, the received data is output from the serial port
-//         if (Nrf24_dataReady(&dev))
-//         {
-//             Nrf24_getData(&dev, buf);
-//             ESP_LOGI(pcTaskGetName(0), "Got data:%s", buf);
-//             // ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(0), buf, payload, ESP_LOG_INFO);
-//         }
-//         vTaskDelay(1);
-//     }
+// 	ESP_LOGW(pcTaskGetName(0), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
+// 	Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
 // }
+// #endif // CONFIG_ADVANCED
 
-
-// void sender(void *pvParameters)
-void sender()
+// #if CONFIG_RECEIVER
+void receiver(void *pvParameters)
 {
     ESP_LOGI(pcTaskGetName(0), "Start");
     NRF24_t dev;
@@ -100,8 +50,8 @@ void sender()
     uint8_t channel = (uint8_t)115;
     Nrf24_config(&dev, channel, payload);
 
-    // Set the receiver address using 5 characters
-    esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");
+    // Set own address using 5 characters
+    esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
     if (ret != ESP_OK)
     {
         ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
@@ -111,7 +61,62 @@ void sender()
         }
     }
 
-    Nrf24_SetSpeedDataRates(&dev, 0);
+#if CONFIG_ADVANCED
+    AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+    // Print settings
+    Nrf24_printDetails(&dev);
+    ESP_LOGI(pcTaskGetName(0), "Listening...");
+
+    uint8_t buf[32];
+
+    // Clear RX FiFo
+    while (1)
+    {
+        if (Nrf24_dataReady(&dev) == false)
+            break;
+        Nrf24_getData(&dev, buf);
+    }
+
+    while (1)
+    {
+        // When the program is received, the received data is output from the serial port
+        if (Nrf24_dataReady(&dev))
+        {
+            Nrf24_getData(&dev, buf);
+            ESP_LOGI(pcTaskGetName(0), "Got data:%s", buf);
+            // ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(0), buf, payload, ESP_LOG_INFO);
+        }
+        vTaskDelay(1);
+    }
+}
+// #endif // CONFIG_RECEIVER
+
+// #if CONFIG_SENDER
+void sender(void *pvParameters)
+{
+    ESP_LOGI(pcTaskGetName(0), "Start");
+    NRF24_t dev;
+    Nrf24_init(&dev);
+    uint8_t payload = 32;
+    uint8_t channel = (uint8_t)100;
+    Nrf24_config(&dev, channel, payload);
+
+    // Set the receiver address using 5 characters
+    esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGIJ");
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+        while (1)
+        {
+            vTaskDelay(1);
+        }
+    }
+
+#if CONFIG_ADVANCED
+    AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
 
     // Print settings
     Nrf24_printDetails(&dev);
@@ -124,7 +129,7 @@ void sender()
         Nrf24_send(&dev, buf);
         vTaskDelay(1);
         ESP_LOGI(pcTaskGetName(0), "Wait for sending.....");
-        if (Nrf24_isSend(&dev, 2000))
+        if (Nrf24_isSend(&dev, 1000))
         {
             ESP_LOGI(pcTaskGetName(0), "Send success:%s", buf);
         }
@@ -132,15 +137,18 @@ void sender()
         {
             ESP_LOGW(pcTaskGetName(0), "Send fail:");
         }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
     }
 }
+// #endif // CONFIG_SENDER
 
 void app_main(void)
 {
-    // vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // #if CONFIG_RECEIVER
+    	// xTaskCreate(&receiver, "RECEIVER", 1024*3, NULL, 2, NULL);
+    // #endif
 
+    // #if CONFIG_SENDER
     xTaskCreate(&sender, "SENDER", 1024 * 3, NULL, 2, NULL);
-    // sender();
-    // xTaskCreate(&receiver, "RECEIVER", 1024 * 3, NULL, 2, NULL);
+    // #endif
 }
